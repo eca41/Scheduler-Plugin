54e6d387077e4881a9042f123449c25cf0b7d4c1
Ivan Elez <ivan.elez@sofascore.com>
2025-03-10 14:56:56 +0100

    a

--- plugin.go
+++ plugin.go
@@ -98,7 +98,7 @@ func (p *MyK3SPlugin) Filter(ctx context.Context, state *framework.CycleState, p
 func (p *MyK3SPlugin) Score(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeName string) (int64, *framework.Status) {
 
 	fmt.Println("--------------------SCORE-------------------------------------")
-	fmt.Println()
+	fmt.Println("Zzzzzzzz	zzzzzzzzzzzzzzzzzzz")
 
 	if p.handle == nil {
 		fmt.Println("Handle is null!")
